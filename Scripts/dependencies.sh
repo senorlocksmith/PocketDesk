424a08d1b072e937c8dec4db64f42e50aea140a9
senorlocksmith <brendan.feltrupexum@mavs.uta.edu>
2024-01-02 11:52:53 -0600

    Update dependencies.sh
    removed dead repo and vala-terminal

--- Scripts/dependencies.sh
+++ Scripts/dependencies.sh
@@ -2,6 +2,5 @@
 
 # install dependencies
 apt-get -y update
-apt-get -y install software-properties-common 
-apt-get -y update
-apt-get -y install git xinput-calibrator xfce4-genmon-plugin awesome apt-transport-https ahoy chip-sunvox pcmanfm pocketchip-batt pocketchip-configs pocketchip-list pocketchip-load pocketchip-localdoc pocketchip-onboard pocketchip-update pocket-home pocket-wm surf vala-terminal
+apt-get --force-yes install software-properties-common 
+apt-get --force-yes install git xinput-calibrator xfce4-genmon-plugin awesome apt-transport-https ahoy chip-sunvox pcmanfm pocketchip-batt pocketchip-configs pocketchip-list pocketchip-load pocketchip-localdoc pocketchip-onboard pocketchip-update pocket-home pocket-wm surf
